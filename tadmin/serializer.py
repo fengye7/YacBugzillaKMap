3434946dbf49a2ff5fa623db74411fad9f26f028
Chengjie Zhou <117179005+fengye7@users.noreply.github.com>
2024-04-16 00:06:42 +0800

    Delete tadmin directory

--- tadmin/serializer.py
+++
@@ -1,8 +0,0 @@
-from tadmin.models import UserInfo
-from rest_framework import serializers
-
-# 序列化
-class UserInfoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserInfo
-        fields = "__all__"
