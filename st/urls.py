e305c1b64c30843b5269209718fbe11a565d10f4
Chengjie Zhou <117179005+fengye7@users.noreply.github.com>
2024-04-16 08:39:59 +0800

    Delete st directory

--- st/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'st'
-urlpatterns = [
-    path('get',views.index,name='index'),
-]
--- st/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(myrequest):
-    # pamdict = {'name':'老铁'}
-    return render(myrequest,'index.html')
